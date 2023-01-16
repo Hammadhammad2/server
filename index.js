a4a34564f10d2fe226d308be0927bd826ecc8b70
Hammad Naeem <hammad.kinectro@gmail.com>
2023-01-16 12:24:19 +0500

    db connection

--- index.js
+++ index.js
@@ -11,6 +11,5 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+dbConnection(app);
 app.use("/", appRoutes);
-
-
